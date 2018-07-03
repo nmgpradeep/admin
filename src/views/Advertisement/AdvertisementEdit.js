1d7e670ed2a89edf4f07e28a249c498ec37657f1
Saurabh Agarwal <saurabh.agarwal@nmg-d-4.newmediaguruorg.com>
2018-07-03 18:16:05 +0530

    Advertisement Module added

---
+++ src/views/Advertisement/AdvertisementEdit.js
@@ -0,0 +1,195 @@
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import axios from 'axios';
+import {
+  Badge,
+  Button,
+  ButtonDropdown,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Col,
+  Collapse,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+  Fade,
+  Form,
+  FormGroup,
+  FormText,
+  FormFeedback,
+  Input,
+  InputGroup,
+  InputGroupAddon,
+  InputGroupText,
+  Label,
+  Row,
+} from 'reactstrap';
+// import PropTypes from 'prop-types';
+class AdvertisementEdit extends Component {
+  constructor(props){
+    super(props);
+    this.advertisemetName = React.createRef();
+    this.description = React.createRef();
+    this.redirectURL = React.createRef();
+    this.image = React.createRef();
+    let advId = this.props.match.params.id;
+    this.state = {
+      editAdv: {},
+      advId: advId,
+      validation:{
+        advertisemetName:{
+          rules: {
+            notEmpty: {
+              message: 'Advertisement name field can\'t be left blank',
+              valid: false
+            }
+          },
+          valid: null,
+          message: ''
+        },
+        description:{
+          rules: {
+            notEmpty: {
+              message: 'Description field can\'t be left blank',
+              valid: false
+            }
+          },
+          valid: null,
+          message: ''
+        },
+        redirectURL: {
+          rules: {
+            notEmpty: {
+              message: 'URL field can\'t be left blank',
+              valid: false
+            },
+          },
+          valid: null,
+          message: ''
+        }
+      }
+    };
+  }
+  cancelHandler(){
+    this.props.history.push("/advertisement");
+  }
+  submitHandler(e){
+      e.preventDefault();
+      let formSubmitFlag = true;
+      for (let field in this.state.validation) {
+        let lastValidFieldFlag = true;
+        let addAdv = this.state.validation;
+        addAdv[field].valid = null;
+        for(let fieldCheck in this.state.validation[field].rules){
+          switch(fieldCheck){
+            case 'notEmpty':
+              if(lastValidFieldFlag === true && this[field].value.length === 0){
+                  lastValidFieldFlag = false;
+                  formSubmitFlag = false;
+                  addAdv[field].valid = false;
+                  addAdv[field].message = addAdv[field].rules[fieldCheck].message;
+
+               }
+              break;
+          }
+        }
+        this.setState({ validation: addAdv});
+      }
+
+      if(formSubmitFlag){
+        let editAdv = this.state.editAdv;
+        editAdv.advertisemetName = this.advertisemetName.value;
+        editAdv.description = this.description.value;
+        editAdv.redirectURL = this.redirectURL.value;
+        editAdv.image = this.image.value;
+        axios.post('/advertisemet/updateAds', editAdv).then(result => {
+          if(result.data.code == '200'){
+            this.props.history.push("/advertisement");
+          }
+        });
+      }
+  }
+
+  componentDidMount() {
+    //if(localStorage.getItem('jwtToken') != null)
+      //axios.defaults.headers.common['Authorization'] = localStorage.getItem('jwtToken');
+      axios.get('/advertisemet/viewAds/' + this.state.advId).then(result => {
+        if(result.data.code == '200'){
+          //localStorage.setItem('jwtToken', result.data.result.accessToken);
+          this.setState({ editAdv: result.data.result});
+          this.advertisemetName.value = result.data.result.advertisemetName;
+          this.description.value = result.data.result.description;
+          this.redirectURL.value = result.data.result.redirectURL;
+          this.image.value = result.data.result.image;
+        }
+      })
+      .catch((error) => {
+        if(error.response.status === 401) {
+          this.props.history.push("/login");
+        }
+      });
+
+  }
+  render() {
+    return (
+      <div className="animated fadeIn">
+        <Row>
+          <Col xs="12" sm="12">
+            <Card>
+              <CardHeader>
+                <strong>User</strong>
+                <small> Edit</small>
+              </CardHeader>
+              <CardBody>
+              <Form noValidate>
+                <Row>
+                  <Col xs="4" sm="12">
+                    <FormGroup>
+                      <Label htmlFor="company">Advertisement Name</Label>
+                      <Input type="text" value={this.state.editAdv.advertisemetName} invalid={this.state.validation.advertisemetName.valid === false} innerRef={input => (this.advertisemetName = input)} placeholder="First name" />
+
+                      <FormFeedback invalid={this.state.validation.advertisemetName.valid === false}>{this.state.validation.advertisemetName.message}</FormFeedback>
+
+                    </FormGroup>
+                    </Col>
+                    <Col xs="4" sm="12">
+                    <FormGroup>
+                      <Label htmlFor="middlename">Description</Label>
+                      <Input type="text" innerRef={input => (this.description = input)} placeholder="Description" />
+                    </FormGroup>
+                    </Col>
+                    <Col xs="4" sm="12">
+                    <FormGroup>
+                      <Label htmlFor="lastname">URL</Label>
+                      <Input type="text" innerRef={input => (this.redirectURL = input)} placeholder="Last name" />
+                    </FormGroup>
+                  </Col>
+                </Row>
+                <FormGroup>
+                  <Label htmlFor="username">Image</Label>
+                  <Input type="file"   innerRef={input => (this.image = input)} placeholder="Image" />
+                  {/* <FormFeedback invalid={this.state.validation.image.valid === false}>{this.state.validation.image.message}</FormFeedback> */}
+                </FormGroup>
+                <Row>
+                  <Col xs="6" className="text-right">
+                    <Button onClick={(e)=>this.submitHandler(e)} color="success" className="px-4">Submit</Button>
+                  </Col>
+                  <Col xs="6">
+                    <Button onClick={()=>this.cancelHandler()} color="primary" className="px-4">Cancel</Button>
+                  </Col>
+                </Row>
+                </Form>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+        </div>
+    );
+  }
+}
+// ProjectItem.propTypes = {
+//   project: PropTypes.object
+// };
+export default AdvertisementEdit;
---
+++ src/views/Advertisement/Advertisements.js
@@ -0,0 +1,167 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Card, CardBody, CardHeader, Col, Pagination, PaginationItem, PaginationLink, Row, Table } from 'reactstrap';
+import axios from 'axios';
+import Advertisement from './Advertisement'
+
+
+class Advertisements extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      advs: [],
+      modal: false,
+      currentPage: 1,
+      PerPage: 5,
+      totalPages: 1,
+      advsCount: 0
+    };
+    console.log('THIS OBJ', this);
+    if(this.props.match.params.page != undefined){
+      this.setState({currentPage: this.props.match.params.page});
+    }
+    this.toggle = this.toggle.bind(this);
+    this.approveDeleteHandler = this.approveDeleteHandler.bind(this);
+  }
+  componentDidMount() {
+    //if(localStorage.getItem('jwtToken') != null)
+      //axios.defaults.headers.common['Authorization'] = localStorage.getItem('jwtToken');
+      axios.get('/advertisemet/list-ads').then(result => {
+        if(result.data.code == '200'){
+          this.setState({
+            advs: result.data.result,
+            currentPage: result.data.current,
+            PerPage: result.data.perPage,
+            totalPages: result.data.pages,
+            advsCount:result.data.total
+          });
+        }
+        console.log(this.state.advs);
+      })
+      .catch((error) => {
+		  console.log('error', error)
+        if(error.response.code === 401) {
+          this.props.history.push("/login");
+        }
+      });
+
+  }
+  advDeleteHandler (id){
+    this.setState({
+      approve: false,
+      approveId: id
+    });
+    this.toggle();
+  }
+  changeStatusHandler(adv){
+    adv.advStatus = (1 - parseInt(adv.advStatus)).toString();
+    axios.post('/advertisemet/updateStatus', adv).then(result => {
+      if(result.data.code === 200){
+        let advs = this.state.advs;
+        let advIndex = advs.findIndex(x => x._id === adv._id);
+        advs[advIndex].advStatus = adv.advStatus.toString();
+        this.setState({ advs: advs});
+      }
+    });
+  }
+  toggle() {
+    this.setState({
+      modal: !this.state.modal
+    });
+  }
+  approveDeleteHandler(){
+    this.setState({
+      approve: true
+    }, function(){
+      if(this.state.approve){
+        axios.delete('/advertisemet/deleteAds/:id' + this.state.approveId).then(result => {
+          if(result.data.code == '200'){
+            let advs = this.state.advs;
+            let advIndex = advs.findIndex(x => x._id === this.state.approveId);
+            advs.splice(advIndex, 1);
+            this.setState({
+              advs: advs,
+              approveId: null,
+              approve: false
+            });
+            this.toggle();
+          }
+        });
+      }
+    });
+  }
+  render() {
+   let advs;
+     if(this.state.advs){
+       let advList = this.state.advs;
+       advs = advList.map(adv => <Advertisement key={adv._id} onDeleteAdv={this.advDeleteHandler.bind(this)} changeStatus={(adv) => this.changeStatusHandler(adv)}   adv={adv}/>);
+     }
+
+     let paginationItems =[];
+
+     const externalCloseBtn = <button className="close" style={{ position: 'absolute', top: '15px', right: '15px' }} onClick={this.toggle}>&times;</button>;
+    return (
+      <div className="animated fadeIn">
+        <Row>
+          <Col>
+            <Card>
+              <CardHeader>
+                <i className="fa fa-align-justify"></i> Advertisements Listing
+                {/* <Link to="/add" className="btn btn-success btn-sm pull-right">Add User</Link> */}
+              </CardHeader>
+              <CardBody>
+                <Table hover bordered striped responsive size="sm">
+                  <thead>
+                  <tr>
+                    {/* <th>ID</th> */}
+                    <th>Name</th>
+                    {/* <th>Description</th> */}
+                    
+                    <th>Logo</th>
+                    
+                    <th>URL</th>
+                    <th>Status</th>
+                    <th>Actions</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                  {advs}
+                  </tbody>
+                </Table>
+                <nav>
+                  <Pagination>
+                    <PaginationItem>
+                      <PaginationLink previous tag="button">Prev</PaginationLink>
+                    </PaginationItem>
+                    {paginationItems}
+
+                    <PaginationItem active>
+                      <PaginationLink tag="button">1</PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem><PaginationLink tag="button">2</PaginationLink></PaginationItem>
+                    <PaginationItem><PaginationLink tag="button">3</PaginationLink></PaginationItem>
+                    <PaginationItem><PaginationLink tag="button">4</PaginationLink></PaginationItem>
+                    <PaginationItem><PaginationLink next tag="button">Next</PaginationLink></PaginationItem>
+                  </Pagination>
+                </nav>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+        <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className} external={externalCloseBtn}>
+          <ModalHeader>Modal title</ModalHeader>
+          <ModalBody>
+            Are you sure to delete?
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={this.approveDeleteHandler}>Yes</Button>{' '}
+            <Button color="secondary" onClick={this.toggle}>No</Button>
+          </ModalFooter>
+        </Modal>
+      </div>
+
+    );
+  }
+}
+
+export default Advertisements;
