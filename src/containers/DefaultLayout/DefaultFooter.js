4be9754777d61423373990f37aa245c226a3eab7
Rajiv <rajiv.kumar.newmediaguru@gmail.com>
2018-07-05 11:25:03 +0530

    cmsPage

--- src/containers/DefaultLayout/DefaultFooter.js
+++ src/containers/DefaultLayout/DefaultFooter.js
@@ -15,7 +15,7 @@ class DefaultFooter extends Component {
 
     return (
       <React.Fragment>
-        <span><a href="https://pitchandswitch.com">PitchAndSwitch</a> &copy; 2018 Swaping Plateform.</span>
+        <span><a href="https://pitchandswitch.com">PitchAndSwitch</a> &copy; 2018 Swapping Plateform.</span>
         <span className="ml-auto">Powered by <a href="http://nmgtechnologies.com">NMG Technology</a></span>
       </React.Fragment>
     );
