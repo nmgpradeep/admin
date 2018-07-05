fa2407e566b4f2ef2d93d08edc0edf614a7fe192
Saurabh Agarwal <saurabh.agarwal@nmg-d-4.newmediaguruorg.com>
2018-07-05 12:33:30 +0530

    remove deleted

--- src/routes/advertisemet.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const advertisemetController = require('../controllers/advertisemetController')
-router.post('/newAds',advertisemetController.create)
-router.get('/list-ads',advertisemetController.advertisemets)
-router.get('/viewAds/:id',advertisemetController.viewAdvertisemet)
-router.put('/updateAds',advertisemetController.updateAdvertisemet)
-router.delete('/deleteAds/:id',advertisemetController.deleteAdvertisemet)
-router.post('/updateStatus',advertisemetController.updateStatus)
-module.exports = router;
